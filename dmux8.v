728258a316e81683ad04234a2ec8b4bd08f5a22a
Piotr Kowalski <piecioshka@gmail.com>
2013-10-27 20:14:15 +0100

    Update dmux8.v

--- dmux8.v
+++ dmux8.v
@@ -1,6 +1,6 @@
 module dmux8
   #(
-    parameter N = 4
+    parameter N = 6
   )
   (
     input [N-1:0] d,
@@ -9,8 +9,9 @@ module dmux8
   );
   
   wire [N-1:0] d0, d1;
-  dmux a (.s({1'b0,s[2]}), .d(d), .y0(d0), .y1(d1));
-  dmux b (.s(s[1:0]), .d(d0), .y0(y0), .y1(y1), .y2(y2), .y3(y3));
-  dmux c (.s(s[1:0]), .d(d1), .y0(y4), .y1(y5), .y2(y6), .y3(y7));
+  // dmux #(.N(N)) - przekazanie do dmux zmiennej lokalnej, w tym przypadku 6
+  dmux #(.N(N)) a (.s({1'b0,s[2]}), .d(d), .y0(d0), .y1(d1));
+  dmux #(.N(N)) b (.s(s[1:0]), .d(d0), .y0(y0), .y1(y1), .y2(y2), .y3(y3));
+  dmux #(.N(N)) c (.s(s[1:0]), .d(d1), .y0(y4), .y1(y5), .y2(y6), .y3(y7));
 
 endmodule
